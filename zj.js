fdce84732ec1322c4161e50c030b2979a9f983f2
yang214 <37854240+yang214@users.noreply.github.com>
2018-05-18 09:47:27 +0800

    创建战舰游戏

---
+++ zj.js
@@ -0,0 +1,45 @@
+var view ={
+  displayMessage:function(msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML =msg;
+  },
+  displayHit: function(location) {
+    var cell = document.getElementById("location");
+    cell.setAttribute("class","hit");
+  },
+  displayMiss:function(location) {
+    cell.setAttribute("class","miss");
+  }
+};
+//view.displayMiss("00");
+//view.displayHit("34");
+//view.displayMiss("55");
+//view.displayHit("12");
+//view.displayMiss("25");
+//view.displayHit("26");
+//view.displayMessage("Tap tap,is this thing on?");
+var model ={
+  boardSize:7,
+  numShips:3,
+  shipLength:3,
+  shipsSunk:0,
+  ships =[ { locations:["06","16","26"],hits:["hit","",""] },
+            { locations:["24","34","44"],hits:["","",""] },
+            { locations:["10","11","12"],hits:["","",""] },
+],
+fire:function(guess) {
+  for (var i =0;i<this.numShips;i++){
+    var ship =this.ships[i];
+    var locations=ship.locations;
+    var index = locations.indexOf(guess);
+    if (index>=0){
+      ship.hits[index] = "hit";
+      return true;
+    }
+  }
+  return false;
+}
+};
+
+
+
