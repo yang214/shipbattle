5fd8994aa764be18e0590cd66101a87e6092f1e5
yang214 <37854240+yang214@users.noreply.github.com>
2018-05-19 22:28:36 +0800

    第一次修改战舰游戏

--- zj.js
+++ zj.js
@@ -1,45 +1,138 @@
 var view ={
-  displayMessage:function(msg) {
-    var messageArea = document.getElementById("messageArea");
-    messageArea.innerHTML =msg;
+    displayMessage:function(msg) {
+      var messageArea = document.getElementById("messageArea");
+      messageArea.innerHTML =msg;
+    },
+    displayHit: function(location) {
+      var cell = document.getElementById("location");
+      cell.setAttribute("class","hit");
+    },
+    displayMiss:function(location) {
+      var cell = document.getElementById("location");
+      cell.setAttribute("class","miss");
+    }
+  };
+  //view.displayMiss("00");
+  //view.displayHit("34");
+  //view.displayMiss("55");
+  //view.displayHit("12");
+  //view.displayMiss("25");
+  //view.displayHit("26");
+  //view.displayMessage("Tap tap,is this thing on?");
+  var ship1 ={locations:["10","20","30"],hits:["","",""]};
+  var ship2 ={locations:["32","33","34"],hits:["","",""]};
+  var ship3 ={locations:["63","64","65"],hits:["","","hit"]};
+
+  var model ={
+    boardSize:7,
+    numShips:3,
+    shipLength:3,
+    shipsSunk:0,
+    ships :[ { locations:["06","16","26"],hits:["","",""] },
+              { locations:["24","34","44"],hits:["","",""] },
+              { locations:["10","11","12"],hits:["","",""] },
+  ],
+  fire:function(guess) {
+    for (var i =0;i<this.numShips;i++){
+      var ship =this.ships[i];
+      var locations=ship.locations;
+      var index = locations.indexOf(guess);
+      if (index>=0){
+        ship.hits[index] = "hit";
+        view.displayHit (guess);
+        view.displayMessage ("HIT");
+        if (this.isSunk(ship)){
+            view.displayMessage ('You sank my battleship!');
+            this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    return false;
   },
-  displayHit: function(location) {
-    var cell = document.getElementById("location");
-    cell.setAttribute("class","hit");
-  },
-  displayMiss:function(location) {
-    cell.setAttribute("class","miss");
+  isSunk: function(ship){
+      for (var i = 0; i<this.shipLength;i++){
+          if (ship.hits[i] !=="hit"){
+            return false;
+          }
+      }
+      return true;
   }
 };
-//view.displayMiss("00");
-//view.displayHit("34");
-//view.displayMiss("55");
-//view.displayHit("12");
-//view.displayMiss("25");
-//view.displayHit("26");
-//view.displayMessage("Tap tap,is this thing on?");
-var model ={
-  boardSize:7,
-  numShips:3,
-  shipLength:3,
-  shipsSunk:0,
-  ships =[ { locations:["06","16","26"],hits:["hit","",""] },
-            { locations:["24","34","44"],hits:["","",""] },
-            { locations:["10","11","12"],hits:["","",""] },
-],
-fire:function(guess) {
-  for (var i =0;i<this.numShips;i++){
-    var ship =this.ships[i];
-    var locations=ship.locations;
-    var index = locations.indexOf(guess);
-    if (index>=0){
-      ship.hits[index] = "hit";
-      return true;
+  model.fire("53");
+
+  model.fire("06");
+  model.fire("16");
+  model.fire("26");
+
+  model.fire("34");
+  model.fire("24");
+  model.fire("44");
+
+  model.fire("12");
+  model.fire("11");
+  model.fire("10");
+//12行、51行、64行
+function parseGuess(guess) {
+  var alphabet=["A","B","C","D","E","F","G"];
+  if (guess ===null||guess.Length!==2){
+    alert("Oops,please enter a letter and a number on the board.");
+  }else{
+    firstChar =guess.charAt(0);
+    var row=alphabet.indexOf(firstChar);
+    var column =guess.charAt(1);
+
+    if (isNaN(row) ||isNaN(column)) {
+      alert("Oops,that isn't on the board.");
+    }else if (row<0||row>=model.boardSize||column<0||column >=model.boardSize){
+      alert("Oops,that's off the board!");
+    }else{
+      return row +column;
+    }  
+  }
+  return null;
+}
+console.log(parseGuess("A0"));
+console.log(parseGuess("B6"));
+console.log(parseGuess("G3"));
+console.log(parseGuess("H0"));
+console.log(parseGuess("A7"));
+var controGuess = {
+  guesses:0,
+  processGuess:function(guess){
+    var location = parseGuess(guess);
+    if (location){
+      this.guesses++;
+      var hit = model.fire(location);
+      if (hit &&model.shipsSunk === model.numShips){
+        view.displayMessage("You sank all my battleships, in"+this.guesses+"guesses");
+      }
     }
   }
-  return false;
-}
 };
+controller.processGuess("A0");
 
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
 
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
 
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
+function init (){
+  var fireButton = document.getElementById("fireButton");
+  fireButton.onclick= handleFireButton;
+}
+function handleFireButton() {
+  var guessInput = document.getElementById("guessInput");
+  var guess = guessInput.value;
+  controller.processGuess(guess);
+  guessInput.value = "";
+}
+window.onload = init;
